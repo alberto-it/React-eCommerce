16c50586ea514ab71b4f015bf8747166973e77fb
alberto-it <56044114+alberto-it@users.noreply.github.com>
2024-05-13 15:24:56 -0400

    Update CreateOrder.jsx

--- src/components/CreateOrder.jsx
+++ src/components/CreateOrder.jsx
@@ -3,39 +3,57 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import CustomerSelection from './CustomerSelection.jsx';
 import ProductSelection from './ProductSelection.jsx';
-import { Modal, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 const CreateOrder = () => {
   const [selectedCustomerId, setSelectedCustomerId] = useState(null);
-  const [selectedProductId, setSelectedProductId] = useState(null);
+  const [selectedProducts, setSelectedProducts] = useState({}); 
+
+  const handleSelectedProducts = (newSelectedProducts) => {
+    setSelectedProducts(newSelectedProducts);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    if (!selectedCustomerId) {
+      toast.error("Please select a customer!");
+      return;
+    }
+
+    const selectedProductIds = Object.keys(selectedProducts).filter(productId => selectedProducts[productId])
+    .map(productId => Number(productId));
+  
+    if (selectedProductIds.length === 0) {
+      toast.error("Please select at least one product!");
+      return;
+    }
+
+    const orderData = {
+      customer_id: selectedCustomerId,
+      product_ids: selectedProductIds,
+    };
+
     try {
-      const orderData = {
-        product_id: selectedProductId,
-        customer_id: selectedCustomerId,
-      };
       const response = await axios.post('http://127.0.0.1:5000/orders', orderData);
-
-      toast.success('Order created successfully!');
-      setTimeout(() => window.location.reload(), 2000);
-
+      toast.success("Order created successfully!");
+      setTimeout(() => window.location.reload(), 2500);
     } catch (error) {
-      console.error('Error creating order:', error.response.data);
-      setError(error.message);
+      console.error("Error creating order:", error);
+      toast.error("Failed to create order!");
     }
   };
 
   return (
+    <div className="order-form-container"> 
     <form onSubmit={handleSubmit}>
       <div className="selections">
         <CustomerSelection onSelectCustomer={setSelectedCustomerId} />
-        <ProductSelection onSelectProduct={setSelectedProductId} />
+        <ProductSelection onSelectProduct={handleSelectedProducts} />
         <Button type="submit" className="btn btn-primary">Create Order</Button>
       </div>
     </form>
+    </div>
   );
 };
 
